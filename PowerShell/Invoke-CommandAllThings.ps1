ce437d7dd6470f7e462d1fe8c1f7519a097bca72
Jason Jarrett <jason@elegantcode.com>
2014-05-17 21:49:28 -0700

    remove debug statements

--- PowerShell/Invoke-CommandAllThings.ps1
+++ PowerShell/Invoke-CommandAllThings.ps1
@@ -8,10 +8,8 @@ function Get-CommandAllThingsProviders {
     $result = @{}
 
     $providers = ls -recurse "$here\providers" -Exclude "*.Tests.ps1" | where { !$_.PSIsContainer }
-    write-host $providers
     $providers | %{
         $name = [System.IO.Path]::GetFileNameWithoutExtension($_);
-        write-host $name
         $result[$name] = (& $_);
     }
     $result
