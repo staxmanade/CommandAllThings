354738e24431fd2a3c68af8e27905780da23f39c
Roman Kuzmin <nightroman@gmail.com>
2014-06-01 13:09:39 +0100

    Added Invoke-Build provider for PowerShell

--- PowerShell/sampleProfile.ps1
+++ PowerShell/sampleProfile.ps1
@@ -4,3 +4,6 @@
 function rake { Invoke-CommandAllThings $args }
 function gulp { Invoke-CommandAllThings $args }
 function grunt { Invoke-CommandAllThings $args }
+
+function Invoke-Build { Invoke-CommandAllThings @args }
+Set-Alias ib Invoke-Build
