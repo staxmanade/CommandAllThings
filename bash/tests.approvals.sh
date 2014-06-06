f960c5c461729c481608b6bf6d3bc92f5027ed7f
Jason Jarrett <jason@elegantcode.com>
2014-06-06 10:05:42 -0700

    adding test wait statement

--- bash/tests.approvals.sh
+++ bash/tests.approvals.sh
@@ -1,9 +1,10 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+echo "Please wait (this may take a moment)..."
+
 # execute the tests ( and also redirect stderr to stdout ) capturing output
 OUTPUT=$($DIR/tests.sh 2>&1 | sed -e 's/after.*μs/after ##.# μs/g')
 
-# remove the gulp timestamps
 
 echo "$OUTPUT"  | approvals tests --outdir $DIR "$@"
