1414f9d1bbaaa6f0c2cb671d11f0813d3e32546f
Jason Jarrett <jason@elegantcode.com>
2014-06-06 10:00:30 -0700

    updating tests

--- bash/tests.sh
+++ bash/tests.sh
@@ -1,28 +1,31 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
+# include the sample aliases
 . $DIR/sampleProfile.sh
 
-
-
-
 commands=( grunt gulp jake rake gradle make )
+
 for i in "${commands[@]}"
 do
+
 	testDir=$(cd "$DIR/../samples/${i}Sample"; pwd)
-	echo "***     Dir: $testDir"
-	echo "*** TESTING: *** $i ***"
+
+	echo "*********************************"
+	echo "*** $i"
+	echo "*** Expected Testing Dir: $testDir"
+
 	cd $testDir
-	echo "***     PWD: $(pwd)"
+
+	echo "*** Current Directory : $(pwd)"
 	
     for j in "${commands[@]}"
     do
 		if [ -e "package.json" ]; then
             npm install > /dev/null
 		fi
-		echo "*** Running: $j"
+		echo "*** Executing: $j"
 	    eval $j
     done
 done
-
 
